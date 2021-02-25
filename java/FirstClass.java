fdd92b6f0919bd01718a1c84d74dd341a290bbc1
zouconghui <569375286@qq.com>
2021-02-25 15:19:55 +0800

    b2

--- java/FirstClass.java
+++ java/FirstClass.java
@@ -3,6 +3,6 @@ public class FirstClass {
         System.out.println(1);
         System.out.println(2);
     }
-    private String b = "b1";
+    private String b = "b2";
     private String a = "a1";
 }
