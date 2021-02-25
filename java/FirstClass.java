d524188bf6f1028debef6c68dc1294d4b51eee85
zouconghui <569375286@qq.com>
2021-02-25 15:06:00 +0800

    d10

--- java/FirstClass.java
+++ java/FirstClass.java
@@ -3,9 +3,5 @@ public class FirstClass {
         System.out.println(1);
         System.out.println(2);
     }
-    private int a = 4;
-    private int b = 6;
-    private int c = 8;
-    private int d = 67;
-	private int f = 12;
+    private String b = "b1";
 }
